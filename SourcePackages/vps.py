b7999133a89ab59ed122e179ddd052387dcf8902
ProjectA31 <64911561+ProjectA31@users.noreply.github.com>
2022-06-12 18:25:16 +0800

    Update vps.py
    hide vps api_key, move to json file

--- SourcePackages/vps.py
+++ SourcePackages/vps.py
@@ -3,12 +3,14 @@ import requests
 import math
 import datetime
 from pdlearn.wechat import WechatHandler
+from pdlearn import file
 
 wechat = WechatHandler()
 
 class vps:
     def get_status():
-        url = 'https://api.64clouds.com/v1/getLiveServiceInfo?veid=1367527&api_key=private_yht2chD7EC2T8G23trvZJk2S'
+        vps_key = file.get_json_data("user/vps_key.json", '{"api_key":""}')      
+        url = 'https://api.64clouds.com/v1/getLiveServiceInfo?veid=1367527&api_key=' + vps_key["api_key"]
         res = requests.get(url).json()
         useage = res.get('data_counter')/1024/1024/1024
         reset = str(datetime.datetime.fromtimestamp(res.get('data_next_reset')))
