65135a53ce97e8440bee42cd4777757730bc9870
ProjectA31 <64911561+ProjectA31@users.noreply.github.com>
2022-06-12 18:05:32 +0800

    Update start.sh
    do not auto run when docker starts and do not add crontab task

--- start.sh
+++ start.sh
@@ -81,7 +81,7 @@ if [ "${Pushmode}" = "6" ]; then
     tail -f /xuexi/user/web_listener.log &
 fi
 
-./run.sh 2>&1 &
-echo -e "$CRONTIME $USER /xuexi/run.sh >> /var/log/cron.log 2>&1\n#empty line" >/etc/cron.d/mycron
-crontab /etc/cron.d/mycron
-cron && tail -f /var/log/cron.log
+#./run.sh 2>&1 &
+#echo -e "$CRONTIME $USER /xuexi/run.sh >> /var/log/cron.log 2>&1\n#empty line" >/etc/cron.d/mycron
+#crontab /etc/cron.d/mycron
+#cron && tail -f /var/log/cron.log
